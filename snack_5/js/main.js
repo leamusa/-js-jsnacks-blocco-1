5dff7d1aade0be3369bfc6c9bd83f85c211dfd41
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-26 13:23:23 +0100

    5

--- snack_5/js/main.js
+++ snack_5/js/main.js
@@ -4,17 +4,17 @@ let numbersOdd = [];
 // -2 Chiedi per 6 volte all'utente di inserire un numero
 for (let i = 0; i < 6; i++) {
   // -3 se è dispari inseriscilo nell’array
-  let numbersUser = parseInt(prompt("Inserisci un numero:"));
+  let userNumber = parseInt(prompt("Inserisci un numero:"));
 
   // Verifica se il numero è dispari
-  while (numeroUtente % 2 === 0) {
+  while (userNumber % 2 === 0) {
     // Se è pari, richiedi il numero
-    numeroUtente = parseInt(prompt("Inserisci un numero dispari:"));
+    userNumber = parseInt(prompt("Inserisci un numero dispari:"));
   }
 
   // Inserisci il numero dispari nell'array
-  numeriDispari.push(numeroUtente);
+  numbersOdd.push(userNumber);
 }
 
 // Stampa l'array con i numeri dispari
-console.log("Numeri dispari inseriti:", numeriDispari);
+console.log("Numeri dispari inseriti:", numbersOdd);
